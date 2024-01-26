d3f45dc823f741c04adba5479641e60006b32805
jedlinks <jedlinks@gmail.com>
2024-01-26 17:22:37 -0600

    Ansible server configurations

--- variables.tf
+++ variables.tf
@@ -1,21 +1,20 @@
 variable "env" {}
+variable "project_name" {}
 variable "kms_key_id" {}
 variable "bastion_cidrs" {}
 
-variable "project_name" {}
 variable "vpc_cidr" {}
 variable "public_subnets_cidr" {}
 variable "web_subnets_cidr" {}
 variable "app_subnets_cidr" {}
 variable "db_subnets_cidr" {}
 variable "az" {}
-
-variable "allocated_storage" {}
-variable "dbname" {}
-variable "engine" {}
-variable "engine_version" {}
-variable "instance_class" {}
-variable "family" {}
+variable "rds_allocated_storage" {}
+variable "rds_dbname" {}
+variable "rds_engine" {}
+variable "rds_engine_version" {}
+variable "rds_instance_class" {}
+variable "rds_family" {}
 
 variable "backend_app_port" {}
 variable "backend_instance_capacity" {}
